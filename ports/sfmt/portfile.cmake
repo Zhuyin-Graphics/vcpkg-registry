aef88a48609d765a4fc9c7664dd09ef8c776efdc
Yiwen Xue <15225434259xue@gmail.com>
2026-03-29 23:46:06 +0800

    Fix sfmt source hash and bump port-version

--- ports/sfmt/portfile.cmake
+++ ports/sfmt/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO MersenneTwister-Lab/SFMT
   REF ${VERSION}
-  SHA512 0
+  SHA512 1f955e2565e3c983338af3c0d180006b92fc428c1197657a0baf3c3dbf963d9ed4942a883cea9846745e2ae9f2eccf06857847db4818fad031a8e98bfb2f045f
   HEAD_REF master
   PATCHES
     add-cmake-support.patch
